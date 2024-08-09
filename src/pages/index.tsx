f2000d6cf2138bb534fa9233962cd4a0ce197f5c
cloutswagsauce <105318787+cloutswagsauce@users.noreply.github.com>
2024-08-09 09:47:56 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,8 +37,10 @@ const App = () => {
       </LazyShow>
       
       <LazyShow>
+        <>
         <VideoSection />
         <SignUp />
+        </>
       </LazyShow>
       
       <LazyShow>
